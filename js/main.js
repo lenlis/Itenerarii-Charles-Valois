8053f81e5c3c2f7cc6539cf671e5f0b179a8b140
lenlis <sit.a.v@mail.ru>
2022-12-12 20:16:53 +0500

    демонстрационное перемещение

--- js/main.js
+++ js/main.js
@@ -17,8 +17,35 @@ $('.mapcontainer').on('mouseenter', function () {
     $.scrollify.enable();
 });
 //Перемещение
-var current_plot;
+var previous_plot;
+var current_plot = 1;
+var falg;
+const dictionary = {
+  1:"Fontenblo",
+  2:"Louvre",
+  3:"Bourges"
+}
 $('#test').on('click', function () {
-  $(".mapcontainer").trigger('zoom', {level: 10, plot:'Fontenblo'});
-  $('svg [data-id="Fontenblo"]').trigger('mouseover')
+  falg = true;
+  previous_plot = current_plot;
+  var updatedOptions = {'plots': {}};
+  updatedOptions.plots[dictionary[current_plot]] = {
+    attrs: {
+        fill: "#3a7bfc"
+    }
+  };
+  if(current_plot > 1){
+  updatedOptions.plots[dictionary[current_plot - 1]] = {
+    attrs: {
+          fill: "#343434"
+      }
+  };
+  }
+  $(".mapcontainer").trigger('zoom', {level: 10, plot:dictionary[current_plot]});
+  $(".mapcontainer").trigger('update', [{
+    mapOptions: updatedOptions
+  }]);
+  if(current_plot < 3){
+    current_plot +=1;
+    }
 });
