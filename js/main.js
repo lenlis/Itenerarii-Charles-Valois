06a399c7c91e9a6291220670c00a9ed20cf979a8
lenlis <sit.a.v@mail.ru>
2022-12-26 14:50:42 +0500

    тест подключения данных

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ $('.mapcontainer').on('mouseenter', function () {
     $.scrollify.enable();
 });
 //Перемещение
+var lines = [];
 $(document).ready(function() {
   $.ajax({
       type: "GET",
@@ -29,15 +30,13 @@ $(document).ready(function() {
 function processData(allText) {
   var allTextLines = allText.split(/\r\n|\n/);
   var headers = allTextLines[0].split(',');
-  var lines = [];
-
   for (var i=1; i<allTextLines.length; i++) {
       var data = allTextLines[i].split(',');
       if (data.length == headers.length) {
 
-          var tarr = [];
+          var tarr = {};
           for (var j=0; j<headers.length; j++) {
-              tarr.push(headers[j]+":"+data[j]);
+              tarr[headers[j]] = data[j];
           }
           lines.push(tarr);
       }
@@ -61,7 +60,7 @@ $('.right').on('click', function () {
   if(current_plot < 9){
     current_plot +=1;
     var updatedOptions = {'plots': {}};
-    updatedOptions.plots[dictionary[current_plot]] = {
+    updatedOptions.plots[dictionary[lines[0]['Место пребывания']]] = {
       size: 25,
       attrs: {
           fill: "#3a7bfc"
