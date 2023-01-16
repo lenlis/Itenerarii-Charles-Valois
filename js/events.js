b3abb5ce4c76c980293838dea055adfbcfdc1349
lenlis <sit.a.v@mail.ru>
2023-01-16 21:30:25 +0500

    убран events.js

--- js/events.js
+++
@@ -1,8 +0,0 @@
-//События
-let popup_event = document.querySelector('.popup-event');
-$('.events').on('click', function () {
-  document.getElementById('popup-event').style = "display: block";
-})
-$('.popup-event-button-exit').on('click', function () {
-  document.getElementById('popup-event').style = "display: none";
-})
